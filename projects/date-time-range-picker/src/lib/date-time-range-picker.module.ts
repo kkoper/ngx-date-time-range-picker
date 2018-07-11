eeeea62e11bc8e71bb1e572d88568ed2ea841db4
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-11 17:01:40 +0200

    Generated month component

--- projects/date-time-range-picker/src/lib/date-time-range-picker.module.ts
+++ projects/date-time-range-picker/src/lib/date-time-range-picker.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DateTimeRangePickerComponent } from './date-time-range-picker.component';
 import { DayComponent } from './day/day.component';
+import { MonthComponent } from './month/month.component';
 
 @NgModule({
   imports: [],
-  declarations: [DateTimeRangePickerComponent, DayComponent],
-  exports: [DateTimeRangePickerComponent, DayComponent]
+  declarations: [DateTimeRangePickerComponent, DayComponent, MonthComponent],
+  exports: [DateTimeRangePickerComponent, DayComponent, MonthComponent]
 })
 export class DateTimeRangePickerModule {}
