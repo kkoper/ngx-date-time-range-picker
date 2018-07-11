9723e36aa2b3982ef8de3943af56b34b5d60d640
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-11 16:55:38 +0200

    Export the DayStatus interface

--- projects/date-time-range-picker/src/public_api.ts
+++ projects/date-time-range-picker/src/public_api.ts
@@ -6,3 +6,4 @@ export * from './lib/date-time-range-picker.component';
 export * from './lib/date-time-range-picker.module';
 export * from './lib/date-time-range-picker.service';
 export * from './lib/day/day.component';
+export * from './lib/models/day-state';
