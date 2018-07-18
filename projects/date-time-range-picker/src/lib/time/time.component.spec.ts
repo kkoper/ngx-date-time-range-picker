6ced34fc18aa355cd5debdeef4b0d644f10ea166
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-18 14:28:59 +0200

    Implemented TimeComponent with tests

--- projects/date-time-range-picker/src/lib/time/time.component.spec.ts
+++ projects/date-time-range-picker/src/lib/time/time.component.spec.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { TimeComponent } from './time.component';
 
 describe('TimeComponent', () => {
@@ -8,18 +10,84 @@ describe('TimeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TimeComponent ]
-    })
-    .compileComponents();
+      imports: [CommonModule, FormsModule],
+      declarations: [TimeComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TimeComponent);
     component = fixture.componentInstance;
+    component.selectedDate = new Date(2018, 6, 26);
+    component.unavailabilities = [
+      { start: new Date(2018, 6, 25, 10, 0), end: new Date(2018, 6, 26, 11, 0) },
+      { start: new Date(2018, 6, 26, 16, 0), end: new Date(2018, 6, 26, 18, 0) },
+      { start: new Date(2018, 6, 26, 23, 0), end: new Date(2018, 6, 27, 11, 0) }
+    ];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show all options in a day', () => {
+    const options = fixture.debugElement.queryAll(By.css('option'));
+
+    expect(options.length).toBe(48);
+  });
+
+  it('should have the correct amount of unavailable options in case of an overlapping start', () => {
+    component.selectedDate = new Date(2018, 6, 26);
+    component.unavailabilities = [
+      { start: new Date(2018, 6, 25, 10, 0), end: new Date(2018, 6, 26, 11, 0) }
+    ];
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const options = fixture.debugElement.queryAll(By.css('option:disabled'));
+
+    expect(options.length).toBe(23);
+  });
+
+  it('should have the correct amount of unavailable options in case of an overlapping end', () => {
+    component.selectedDate = new Date(2018, 6, 26);
+    component.unavailabilities = [
+      { start: new Date(2018, 6, 26, 23, 0), end: new Date(2018, 6, 27, 11, 0) }
+    ];
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const options = fixture.debugElement.queryAll(By.css('option:disabled'));
+
+    expect(options.length).toBe(2);
+  });
+
+  it('should have the correct amount of unavailable options in case of a period within the day', () => {
+    component.selectedDate = new Date(2018, 6, 26);
+    component.unavailabilities = [
+      { start: new Date(2018, 6, 26, 16, 0), end: new Date(2018, 6, 26, 18, 0) }
+    ];
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const options = fixture.debugElement.queryAll(By.css('option:disabled'));
+
+    expect(options.length).toBe(5);
+  });
+
+  it('should emit when a time is selected', () => {
+    spyOn(component.timeSelected, 'emit').and.callThrough();
+
+    const timeToSelect = { hour: 23, minute: 30, isBlocked: false };
+    component.selectedTimeOption = timeToSelect;
+
+    expect(component.timeSelected.emit).toHaveBeenCalledWith({
+      hours: timeToSelect.hour,
+      minutes: timeToSelect.minute
+    });
   });
 });
--- projects/date-time-range-picker/src/lib/time/time.component.ts
+++ projects/date-time-range-picker/src/lib/time/time.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Time } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import * as moment_ from 'moment';
+import { DateTimeRange } from '../models/date-time-range';
+
+const moment = moment_;
 
 @Component({
   selector: 'ngx-time',
@@ -6,10 +11,146 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time.component.scss']
 })
 export class TimeComponent implements OnInit {
+  @Input() unavailabilities: DateTimeRange[];
+  @Input() selectedDate: Date;
+  @Output() timeSelected = new EventEmitter<Time>();
 
-  constructor() { }
+  private _selectedTimeOption: TimeSegment;
+  get selectedTimeOption(): TimeSegment {
+    return this._selectedTimeOption;
+  }
+  set selectedTimeOption(value: TimeSegment) {
+    this._selectedTimeOption = value;
+    this.timeSelected.emit({ hours: value.hour, minutes: value.minute });
+  }
+  timeOptions: TimeSegment[];
+
+  constructor() {}
 
   ngOnInit() {
+    this.initializeTimeSegments();
+    this.applyUnavailabilities();
   }
 
+  private applyUnavailabilities(): void {
+    for (const unavailability of this.unavailabilities) {
+      const startMoment = moment(unavailability.start);
+      const endMoment = moment(unavailability.end);
+      const currentDay = moment(this.selectedDate);
+
+      if (this.isSameDay(startMoment, currentDay)) {
+        if (this.isSameDay(endMoment, currentDay)) {
+          this.handleUnavailabilityWithinTheDay(unavailability);
+        } else {
+          this.handleUnavailabilityOverlappingEndOfDay(unavailability);
+        }
+      } else {
+        if (this.isSameDay(endMoment, currentDay)) {
+          this.handleUnavailabilityOverlappingBeginOfDay(unavailability);
+        } else {
+          this.handleUnavailabilityOverlappingFullDay(unavailability);
+        }
+      }
+    }
+  }
+
+  private handleUnavailabilityWithinTheDay(unavailability: DateTimeRange): any {
+    const startTime: Time = {
+      hours: unavailability.start.getHours(),
+      minutes: unavailability.start.getMinutes()
+    };
+    const endTime: Time = {
+      hours: unavailability.end.getHours(),
+      minutes: unavailability.end.getMinutes()
+    };
+    this.makeTimesBlocked(startTime, endTime);
+  }
+
+  private handleUnavailabilityOverlappingEndOfDay(unavailability: DateTimeRange): any {
+    const startTime: Time = {
+      hours: unavailability.start.getHours(),
+      minutes: unavailability.start.getMinutes()
+    };
+    const endTime: Time = {
+      hours: 23,
+      minutes: 59
+    };
+    this.makeTimesBlocked(startTime, endTime);
+  }
+
+  private handleUnavailabilityOverlappingBeginOfDay(unavailability: DateTimeRange): any {
+    const startTime: Time = {
+      hours: 0,
+      minutes: 0
+    };
+    const endTime: Time = {
+      hours: unavailability.end.getHours(),
+      minutes: unavailability.end.getMinutes()
+    };
+    this.makeTimesBlocked(startTime, endTime);
+  }
+
+  private handleUnavailabilityOverlappingFullDay(unavailability: DateTimeRange): any {
+    const startTime: Time = {
+      hours: 0,
+      minutes: 0
+    };
+    const endTime: Time = {
+      hours: 23,
+      minutes: 59
+    };
+    this.makeTimesBlocked(startTime, endTime);
+  }
+
+  private makeTimesBlocked(startTime: Time, endTime: Time): any {
+    this.timeOptions = this.timeOptions.map((timeOption: TimeSegment) => {
+      if (timeOption.hour === startTime.hours && timeOption.minute >= startTime.minutes) {
+        timeOption.isBlocked = true;
+      } else if (timeOption.hour === endTime.hours && timeOption.minute <= endTime.minutes) {
+        timeOption.isBlocked = true;
+      } else if (timeOption.hour > startTime.hours && timeOption.hour < endTime.hours) {
+        timeOption.isBlocked = true;
+      }
+
+      return timeOption;
+    });
+  }
+
+  private isSameDay(firstDate: moment_.Moment, secondDate: moment_.Moment): boolean {
+    if (
+      firstDate.year() === secondDate.year() &&
+      firstDate.month() === secondDate.month() &&
+      firstDate.day() === secondDate.day()
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  private initializeTimeSegments(): void {
+    const times: TimeSegment[] = [];
+    for (let i = 0; i < 24; i++) {
+      const fixedTimeSegment: TimeSegment = {
+        hour: i,
+        minute: 0,
+        isBlocked: false
+      };
+      const halfTimeSegment: TimeSegment = {
+        hour: i,
+        minute: 30,
+        isBlocked: false
+      };
+
+      times.push(fixedTimeSegment);
+      times.push(halfTimeSegment);
+    }
+
+    this.timeOptions = [...times];
+  }
+}
+
+export interface TimeSegment {
+  hour: number;
+  minute: number;
+  isBlocked: boolean;
 }
