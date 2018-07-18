df811d055267413e8b9f11de063d6c595420de7d
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-18 14:29:45 +0200

    Demo use of the TimeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Time } from '@angular/common';
 import { Component } from '@angular/core';
 import { DateTimeRange, DayState } from 'date-time-range-picker';
 
@@ -11,13 +12,18 @@ export class AppComponent {
 
   month = 6;
   year = 2018;
+  hoverFrom = new Date(2018, 6, 17);
   unavailability: DateTimeRange[] = [
     // { start: new Date(2018, 6, 14, 10, 0), end: new Date(2018, 6, 14, 21, 0) },
     // { start: new Date(2018, 6, 16, 10, 0), end: new Date(2018, 6, 18, 18, 0) },
     // { start: new Date(2018, 6, 18, 10, 0), end: new Date(2018, 6, 21, 21, 0) },
-    { start: new Date(2018, 6, 26, 10, 0), end: new Date(2018, 6, 29, 21, 0) }
+    { start: new Date(2018, 6, 25, 10, 0), end: new Date(2018, 6, 26, 11, 0) },
+    { start: new Date(2018, 6, 26, 16, 0), end: new Date(2018, 6, 26, 18, 0) },
+    { start: new Date(2018, 6, 26, 23, 0), end: new Date(2018, 6, 27, 11, 0) }
   ];
-  hoverFrom = new Date(2018, 6, 17);
+
+  // For the time component
+  selectedDate = new Date(2018, 6, 26);
 
   onDateSelected(date: Date) {
     this.hoverFrom = date;
@@ -26,6 +32,10 @@ export class AppComponent {
 
   onDaySelected(day: number) {
     console.log('day selected', day);
+  }
+
+  onTimeSelected(time: Time) {
+    console.log('time selected', time);
   }
 
   onDayHovered(day: number) {
