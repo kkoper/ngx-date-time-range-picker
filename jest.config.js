628200ecb7fabc0a9ab69ad6ae9add92db25ffb8
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-19 15:06:42 +0200

    Added Jest and Docs

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['projects/date-time-range-picker/src'],
+  setupTestFrameworkScriptFile: '<rootDir>/setup-jest.ts',
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/projects/date-time-range-picker/src/lib/$1',
+    '@env': '<rootDir>/src/environments/environment',
+    '@src/(.*)': '<rootDir>/projects/date-time-range-picker/src/$1'
+  }
+};
