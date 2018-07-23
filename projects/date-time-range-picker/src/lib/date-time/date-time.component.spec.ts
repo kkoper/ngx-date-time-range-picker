1da9a8da0eeaf4709be5518631ad532c833dbb56
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-23 13:05:49 +0200

    The previous button now goes to previous month

--- projects/date-time-range-picker/src/lib/date-time/date-time.component.spec.ts
+++ projects/date-time-range-picker/src/lib/date-time/date-time.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
+import * as moment_ from 'moment';
 import { DayComponent } from '../day/day.component';
 import { MonthComponent } from '../month/month.component';
 import { TimeComponent } from '../time/time.component';
 import { DateTimeComponent } from './date-time.component';
+const moment = moment_;
 
 describe('DateTimeComponent', () => {
   let component: DateTimeComponent;
@@ -39,12 +41,40 @@ describe('DateTimeComponent', () => {
     expect(timeComponent.length).toBe(1);
   });
 
+  it('should have the current year and month as a default selection', () => {
+    const currentYear = moment().year();
+    const currentMonth = moment().month();
+
+    expect(component.year).toBe(currentYear);
+    expect(component.currentMonth).toBe(currentMonth);
+  });
+
   it('should go to next month', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should show the current month and year', () => {
+    const currentYear = moment().year();
+    const currentMonth = moment().format('MMMM');
+    expect(fixture.debugElement.nativeElement.innerHTML).toContain(currentYear);
+    expect(fixture.debugElement.nativeElement.innerHTML).toContain(currentMonth);
+  });
+
   it('should go to previous month', () => {
-    expect(component).toBeTruthy();
+    const previousMonth = moment()
+      .subtract(1, 'months')
+      .month();
+    const previousYear = moment()
+      .subtract(1, 'months')
+      .year();
+    const previousMonthButton = fixture.debugElement.queryAll(By.css('.btn-previous-month'));
+    expect(previousMonthButton.length).toBe(1);
+
+    previousMonthButton[0].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.currentMonth).toBe(previousMonth);
+    expect(component.year).toBe(previousYear);
   });
 
   it('should give month unavailabilities', () => {
--- projects/date-time-range-picker/src/lib/date-time/date-time.component.ts
+++ projects/date-time-range-picker/src/lib/date-time/date-time.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment_ from 'moment';
 import { DateTimeRange } from '../models/date-time-range';
+const moment = moment_;
 
 @Component({
   selector: 'ngx-date-time',
@@ -8,15 +10,37 @@ import { DateTimeRange } from '../models/date-time-range';
 })
 export class DateTimeComponent implements OnInit {
   // Month component needs:
-  year = 2018;
-  currentMonth = 6;
+  year: number;
+  currentMonth: number;
   monthUnavailabilities: DateTimeRange[] = [];
 
   // Time component needs:
   timeUnavailabilities: DateTimeRange[] = [];
   selectedDayMonth: Date = new Date();
 
+  // Computed values:
+  get monthName(): string {
+    return moment()
+      .month(this.currentMonth)
+      .format('MMMM');
+  }
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.year = moment().year();
+    this.currentMonth = moment().month();
+  }
+
+  goToPreviousMonth(): void {
+    const previousMonth = moment()
+      .subtract(1, 'months')
+      .month();
+    const previousYear = moment()
+      .subtract(1, 'months')
+      .year();
+
+    this.year = previousYear;
+    this.currentMonth = previousMonth;
+  }
 }
