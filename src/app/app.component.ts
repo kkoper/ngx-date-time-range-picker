a58a26498ac402b8556b7ee972fd42ee4d162511
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-07-23 16:46:55 +0200

    App component correctly gived different unavailabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DateTimeRange } from 'date-time-range-picker';
 
 @Component({
@@ -6,21 +6,51 @@ import { DateTimeRange } from 'date-time-range-picker';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  unavailability: DateTimeRange[] = [
-    { start: new Date(2018, 9, 14, 10, 0), end: new Date(2018, 9, 14, 21, 0) },
-    { start: new Date(2018, 9, 16, 10, 0), end: new Date(2018, 9, 18, 18, 0) },
-    { start: new Date(2018, 9, 18, 10, 0), end: new Date(2018, 9, 21, 21, 0) },
-    { start: new Date(2018, 9, 25, 10, 0), end: new Date(2018, 9, 26, 11, 0) },
-    { start: new Date(2018, 9, 26, 16, 0), end: new Date(2018, 9, 26, 18, 0) },
-    { start: new Date(2018, 9, 26, 23, 0), end: new Date(2018, 9, 27, 11, 0) }
+export class AppComponent implements OnInit {
+  unavailability: DateTimeRange[][] = [
+    [
+      { start: new Date(2019, 0, 14, 10, 0), end: new Date(2019, 0, 14, 21, 0) },
+      { start: new Date(2019, 0, 16, 10, 0), end: new Date(2019, 0, 18, 18, 0) },
+      { start: new Date(2019, 0, 26, 16, 0), end: new Date(2019, 0, 26, 18, 0) },
+      { start: new Date(2019, 0, 26, 23, 0), end: new Date(2019, 0, 27, 11, 0) }
+    ],
+    [{ start: new Date(2019, 1, 26, 23, 0), end: new Date(2019, 1, 27, 11, 0) }],
+    [{ start: new Date(2019, 2, 26, 23, 0), end: new Date(2019, 2, 27, 11, 0) }],
+    [
+      { start: new Date(2019, 3, 18, 10, 0), end: new Date(2019, 3, 21, 21, 0) },
+      { start: new Date(2019, 3, 25, 10, 0), end: new Date(2019, 3, 26, 11, 0) },
+      { start: new Date(2019, 3, 26, 16, 0), end: new Date(2019, 3, 26, 18, 0) },
+      { start: new Date(2019, 3, 26, 23, 0), end: new Date(2019, 3, 27, 11, 0) }
+    ],
+    [
+      { start: new Date(2019, 4, 14, 10, 0), end: new Date(2019, 4, 14, 21, 0) },
+      { start: new Date(2019, 4, 16, 10, 0), end: new Date(2019, 4, 18, 18, 0) },
+      { start: new Date(2019, 4, 18, 10, 0), end: new Date(2019, 4, 21, 21, 0) }
+    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
   ];
+  monthUnavailability: DateTimeRange[];
+
+  ngOnInit() {
+    this.monthUnavailability = this.unavailability[new Date().getMonth()];
+  }
 
   onDateTimeSelected(date: Date) {
     console.log('date-time selected', date);
   }
 
   onMonthChanged(date: Date) {
+    this.monthUnavailability = this.unavailability[date.getMonth()];
     console.log('month changed', date);
   }
 }
